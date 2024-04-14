93b8c5ed7b9450bfb269adbcff548fae673ad5cf
KilDReaper <aayam2074@gmail.com>
2024-04-14 21:10:36 +0545

    task 6

--- week 1/Hello.java
+++ week 1/Hello.java
@@ -2,12 +2,17 @@ public class Hello {
     
     public static void main(String[] args){
         System.out.println("Hello world");
-        System.out.println("\n This is my first program");
+        System.out.println("\nThis is my first program");
         System.out.print("\nI am on module ST4003CEM\n");
         System.out.println("\nA \"quoted\" String is\n\'much\' better if you learn \nthe rules of \"escape sequences\" \nAlso, \"\"represents an empty \nString. Don't forget: use \n\\\"instead of \" ! \n\"is not the same as\"");
         System.out.println("\n*");
         System.out.println("**");
         System.out.println("***");
         System.out.println("****");
+        System.out.println("\n*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("\nA well-\nformed \nJava \nprogram \nhas a \nmain \nmethod \nwith {and\n} braces.\n\nA \nSystem.o\nut.println \nstatement \nhas(and\n) and \nusually a \nString \nthat starts \nand ends \nwith a \" \ncharacter.\n(But we type \\\"instead!)\nWhat is the difference between \na \' and a\"? Or between a\" and a \'?\n\nOne is what we see\nwhen we're typing our \nprogram. The other is \nwhat appears on the \"console.\"");
     }
 }
