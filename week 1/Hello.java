43872708976f7912ca0ce5c0d48bdf6b46ee32f8
KilDReaper <aayam2074@gmail.com>
2024-04-14 12:28:09 +0545

    task2

--- week 1/Hello.java
+++ week 1/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("\n This is my first program");
+        System.out.print("\nI am on module ST4003CEM");
     }
 }
