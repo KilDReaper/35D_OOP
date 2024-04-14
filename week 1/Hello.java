6ee11fa02ace933a37a54c9e1b69c86423afce13
KilDReaper <aayam2074@gmail.com>
2024-04-14 12:44:48 +0545

    task 2

--- week 1/Hello.java
+++ week 1/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("\n This is my first program");
-        System.out.print("\nI am on module ST4003CEM");
+        System.out.print("\nI am on module ST4003CEM\n");
+        System.out.println("\nA \"quoted\" String is\n\'much\' better if you learn \nthe rules of \"escape sequences\" \nAlso, \"\"represents an empty \nString. Don't forget: use \n\\\"instead of \" ! \n\"is not the same as\"");
     }
 }
